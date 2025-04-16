f08a2ca21e9d313d085cdcddae2a6afebba96371
elisa <elisavc@mail.ru>
2025-04-16 22:08:36 +0300

    штшешфд сщььше

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+"use strict";
+
+function solveEquation (a, b, c) {
+	let arr = [];
+	let d = (b ** 2) - 4 * a * c;
+	if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		let x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+function calculateTotalMortgage (percent, contribution, amount, countMonths) {
+	let interestRate = (percent / 100) / 12;
+	let creditBody = amount - contribution;
+	let months = countMonths;
+
+	let monthsPay = creditBody * (interestRate + (interestRate / (((1 + interestRate) ** months) - 1)));
+	let result = monthsPay * months;
+	result.toFixed(2);
+	return Number (result.toFixed(2));
 }
+
